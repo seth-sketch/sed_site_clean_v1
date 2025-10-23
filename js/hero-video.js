78cbd93f80d1c64ba69c74e7f6161abe3b9accdd
seth-sketch <seth@setheasterdesign.com>
2025-10-23 17:43:18 -0400

    maybe

---
+++ js/hero-video.js
@@ -0,0 +1,1 @@
+(function(){var v=document.getElementById('heroVideo');if(!v)return;function tryPlay(){var p;try{p=v.play();}catch(e){}if(p&&p.then)p.catch(function(){});}v.addEventListener('loadeddata',tryPlay);v.addEventListener('canplay',tryPlay);window.addEventListener('pageshow',tryPlay);document.addEventListener('visibilitychange',function(){if(!document.hidden)tryPlay();});tryPlay();})();
